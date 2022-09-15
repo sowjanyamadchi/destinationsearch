5ded92580771981fb05e97f2a386218d0892f04d
sowjanya madchi <sowjanya.madchi1@gmail.com>
2022-09-15 06:11:37 +0000

    destinationsearch

---
+++ src/components/DestinationItem/index.js
@@ -0,0 +1,15 @@
+// Write your code here
+import './index.css'
+
+const DestinationItem = props => {
+  const {name, imgUrl} = props
+  return (
+    <li className="image-container">
+      <img className="place-image" src={imgUrl} alt={name} />
+
+      <p className="place-name">{name}</p>
+    </li>
+  )
+}
+
+export default DestinationItem
